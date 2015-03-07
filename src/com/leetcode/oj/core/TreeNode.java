c6c6c6cc9145b03ce44b712182e5e1013989c35d
Situ Ma <situ.ma@maxsitu.com>
2015-03-07 15:38:46 -0500

    Solution for "95. Unique Binary Search Trees II".

--- src/com/leetcode/oj/core/TreeNode.java
+++ src/com/leetcode/oj/core/TreeNode.java
@@ -1,5 +1,7 @@
 package com.leetcode.oj.core;
 
+import java.util.Stack;
+
 /**
  * Created by appleowner on 2/14/15.
  */
@@ -22,5 +24,43 @@ public class TreeNode {
         TreeNode parent = new TreeNode(pVal);
         parent.left = this;
         return parent;
+    }
+
+    public static String preOrder(TreeNode p) {
+        StringBuffer sb = new StringBuffer();
+        Stack<TreeNode> pStack = new Stack<TreeNode>();
+        while (p != null) {
+            pStack.add(p);
+            p = p.left;
+        }
+        while (p != null || !pStack.isEmpty()) {
+            if (p == null) {
+                p = pStack.pop();
+            } else {
+                sb.append(p.val + " ");
+                p = p.right;
+                if (p != null) sb.append("# ");
+                while (p != null) {
+                    pStack.add(p);
+                    p = p.left;
+                }
+            }
+        }
+        return sb.toString();
+    }
+
+    public static void main(String[] args) {
+        TreeNode n1 = new TreeNode(1);
+        TreeNode n2 = new TreeNode(2);
+        TreeNode n3 = new TreeNode(3);
+        TreeNode n4 = new TreeNode(4);
+        TreeNode n5 = new TreeNode(5);
+
+        TreeNode p = n4;
+        p.left = n2;
+        p.right = n5;
+        n2.left = n1;
+        n2.right = n3;
+        System.out.println(TreeNode.preOrder(p));
     }
 }
