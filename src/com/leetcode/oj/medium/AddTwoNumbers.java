dea502bc6c6dd4a93e1dc209ef106a46ca92f3d0
Situ Ma <situ.ma@maxsitu.com>
2015-03-26 00:35:13 -0400

    Solution for "2. Add Two Numbers". zen-1

---
+++ src/com/leetcode/oj/medium/AddTwoNumbers.java
@@ -0,0 +1,27 @@
+package com.leetcode.oj.medium;
+
+import com.leetcode.oj.core.ListNode;
+
+/**
+ * You are given two linked lists representing two non-negative numbers. The digits are stored in reverse order and each of their nodes contain a single digit. Add the two numbers and return it as a linked list.
+ * <p/>
+ * Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
+ * Output: 7 -> 0 -> 8
+ */
+public class AddTwoNumbers {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        int carry = 0;
+        ListNode dummy = new ListNode(0);
+        ListNode curr = dummy;
+        while (l1 != null || l2 != null || carry > 0) {
+            int val = (l1 == null ? 0 : l1.val) + (l2 == null ? 0 : l2.val) + carry;
+            carry = val / 10;
+            val = val % 10;
+            curr.next = new ListNode(val);
+            curr = curr.next;
+            if (l1 != null) l1 = l1.next;
+            if (l2 != null) l2 = l2.next;
+        }
+        return dummy.next;
+    }
+}
