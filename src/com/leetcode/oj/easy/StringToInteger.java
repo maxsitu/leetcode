b0a9c141a47508c3d2b25e3c909fa1e2da7e4803
Situ Ma <situ.ma@maxsitu.com>
2015-03-31 00:42:07 -0400

    Solution to "8. String to Integer (atoi)" zen-1

---
+++ src/com/leetcode/oj/easy/StringToInteger.java
@@ -0,0 +1,60 @@
+package com.leetcode.oj.easy;
+
+/**
+ * Created by WallE on 2015/3/31.
+ */
+public class StringToInteger {
+    public static void main(String[] args) {
+        System.out.println(new StringToInteger().atoi("  -34567"));
+    }
+
+    public int atoi(String str) {
+        char[] cArr = str.toCharArray();
+        int idx = 0;
+        boolean positive = true;
+        long result = 0;
+        if (cArr.length == 0) return 0;
+        while (cArr[idx] == ' ') idx++;
+        if (cArr[idx] == '+') idx++;
+        else if (cArr[idx] == '-') {
+            positive = false;
+            idx++;
+        }
+        while (idx < cArr.length && cArr[idx] >= '0' && cArr[idx] <= '9') {
+            if (positive) {
+                result = result * 10 + cArr[idx] - '0';
+                if (result > Integer.MAX_VALUE) return Integer.MAX_VALUE;
+            } else {
+                result = result * 10 - cArr[idx] + '0';
+                if (result < Integer.MIN_VALUE) return Integer.MIN_VALUE;
+            }
+            idx++;
+        }
+        return (int) result;
+    }
+
+    public int atoi(String str) {
+        char[] cArr = str.toCharArray();
+        int idx = 0;
+        boolean positive = true;
+        long result = 0;
+        if (cArr.length == 0) return 0;
+        while (cArr[idx] == ' ') idx++;
+        if (cArr[idx] == '+') idx++;
+        else if (cArr[idx] == '-') {
+            positive = false;
+            idx++;
+        }
+        while (idx < cArr.length && cArr[idx] >= '0' && cArr[idx] <= '9') {
+            if (positive) {
+                result = result * 10 + cArr[idx] - '0';
+                if (result > Integer.MAX_VALUE) return Integer.MAX_VALUE;
+            } else {
+                result = result * 10 - cArr[idx] + '0';
+                if (result < Integer.MIN_VALUE) return Integer.MIN_VALUE;
+            }
+            idx++;
+        }
+        return (int) result;
+    }
+}
