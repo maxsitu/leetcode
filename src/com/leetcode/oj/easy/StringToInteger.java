9227db24d4fef323bc6940bd213726d961bb646f
Situ Ma <situ.ma@maxsitu.com>
2015-03-31 00:43:49 -0400

    Solution to "8. String to Integer (atoi)" zen-1

--- src/com/leetcode/oj/easy/StringToInteger.java
+++ src/com/leetcode/oj/easy/StringToInteger.java
@@ -32,29 +32,4 @@ public class StringToInteger {
         }
         return (int) result;
     }
-
-    public int atoi(String str) {
-        char[] cArr = str.toCharArray();
-        int idx = 0;
-        boolean positive = true;
-        long result = 0;
-        if (cArr.length == 0) return 0;
-        while (cArr[idx] == ' ') idx++;
-        if (cArr[idx] == '+') idx++;
-        else if (cArr[idx] == '-') {
-            positive = false;
-            idx++;
-        }
-        while (idx < cArr.length && cArr[idx] >= '0' && cArr[idx] <= '9') {
-            if (positive) {
-                result = result * 10 + cArr[idx] - '0';
-                if (result > Integer.MAX_VALUE) return Integer.MAX_VALUE;
-            } else {
-                result = result * 10 - cArr[idx] + '0';
-                if (result < Integer.MIN_VALUE) return Integer.MIN_VALUE;
-            }
-            idx++;
-        }
-        return (int) result;
-    }
 }
